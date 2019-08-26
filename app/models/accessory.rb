e69bdcd2088b3796389a9c005df3ca678b1276cb
Florence Schwartz <schflo@gmail.com>
2019-08-26 21:52:21 +0200

    changed arenas views, added cascade for accessories

--- app/models/accessory.rb
+++ app/models/accessory.rb
@@ -1,5 +1,8 @@
 class Accessory < ApplicationRecord
-    has_many :arena
+    has_many :arenas, ->(accessory) do
+        unscope(where: :character_1_weapon)
+        .where('character_1_weapon_id = :id OR character_1_weapon_id = :id', id: accessory.id)
+    end, dependent: :destroy
     validates :weapon, numericality: { greater_than_or_equal_to: 0}
     validates :shield, numericality: { greater_than_or_equal_to: 0}
 end
--- app/models/character.rb
+++ app/models/character.rb
@@ -1,7 +1,13 @@
 class Character < ApplicationRecord
     has_one_attached :avatar
-    has_many :arena
-    has_many :accessory
+    has_many :arenas, ->(character) do
+        unscope(where: :character_id)
+        .where('character_1_id = :id OR character_2_id = :id', id: character.id)
+    end, dependent: :destroy
+    has_many :accessories, ->(accessory) do
+        unscope(where: :accessory_id)
+        .where('character_1_weapon_id = :id OR character_2_weapon_id = :id', id: accessory.id)
+    end, dependent: :destroy
 
     def update_stats
         total_matches = self.defeat + self.victory
